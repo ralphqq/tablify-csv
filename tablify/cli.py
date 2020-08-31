c7071d7e1b230c9eb3d34a83ae769973241e8300
Ralph Quirequire <ralph.quirequire@outlook.com>
2020-08-31 22:33:48 +0800

    Add CLI and runner modules

---
+++ tablify/cli.py
@@ -0,0 +1,37 @@
+"""
+CLI Command
+"""
+import asyncio
+import logging
+
+import typer
+
+from .reader import CSVTableReader
+from .renderer import HTMLRenderer
+from .ui import UserColumnSettings
+
+
+logger = logging.getLogger(__name__)
+
+app = typer.Typer()
+
+@app.command('tablify')
+def tablify(csv_file: str, output_file: str = '') -> None:
+    try:
+        csv_table = CSVTableReader(csv_file)
+        user_input = UserColumnSettings(csv_table.columns)
+        user_input.get_input()
+
+        html_renderer = HTMLRenderer(csv_table.data, user_input)
+        html_table = asyncio.run(html_renderer.render())
+
+        fpath = output_file if output_file else f'{csv_file}.html'
+        with open(fpath, 'w', encoding='utf-8') as f:
+            f.write(html_table)
+
+        logger.info(f'Finished writing converted table to {fpath}')
+    except KeyboardInterrupt:
+        logger.info('Canceled')
+
+    except Exception as e:
+        logger.error(f'An unhandled error occurred: {e}')
---
+++ tablify/runner.py
@@ -0,0 +1,17 @@
+"""
+Script entry point
+"""
+import logging
+
+from .cli import app
+
+LOGLEVEL = 'INFO'
+
+
+def main():
+logging.basicConfig(
+        format='%(message)s',
+        level=getattr(logging, LOGLEVEL)
+    )
+    logging.debug('Initializing')
+    app()
