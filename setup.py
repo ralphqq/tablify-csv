994d8f9e22572496ee706a2aa960419270bf1e51
Ralph Quirequire <ralph.quirequire@outlook.com>
2020-09-04 23:29:12 +0800

    Package the project

---
+++ setup.py
@@ -0,0 +1,65 @@
+import re
+from setuptools import setup
+
+# Helper functions
+
+def get_info():
+    """Returns tool's details as a dict."""
+    info = None
+    with open('tablify/__init__.py', 'r', encoding='utf-8') as f:
+        info = dict(re.findall(
+            r"__(\w+)__ = '([^']+)'$",
+            f.read(),
+            re.MULTILINE
+        ))
+
+    with open('README.md', 'r', encoding='utf-8') as g:
+        info['long_description'] = g.read()
+
+    return info
+
+# Details
+
+INFO = get_info()
+REQUIRES = [
+    'lxml==4.5.2',
+    'prompt-toolkit==3.0.6',
+    'typer==0.3.2',
+]
+TEST_REQUIRES = [
+    'pytest>=5.4.3',
+    'pytest-asyncio>=0.12.0',
+    'pytest-mock>=3.1.1',
+]
+PACKAGES = ['tablify']
+
+
+# Setup
+
+setup(
+    name=INFO['title'],
+    version=INFO['version'],
+    description=INFO['short_description'],
+    long_description=INFO['long_description'],
+    long_description_content_type="text/markdown",
+    author=INFO['author'],
+    author_email=INFO['author_email'],
+    license=INFO['license'],
+    url=INFO['url'],
+    packages=PACKAGES,
+    install_requires=REQUIRES,
+    extras_require={'tests': TEST_REQUIRES},
+    python_requires='>=3.8',
+    entry_points={'console_scripts': ['tablify-csv = tablify.runner:main']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    keywords=(
+        'csv html table conversion'
+        'formatter converter parser'
+    ),
+)
