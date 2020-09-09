8077a84157bf14cf542f4661af16b7cb32119f64
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-09-09 22:16:37 +0800

    Skip autoformatting for package info items in __init__.py

--- tablify/__init__.py
+++ tablify/__init__.py
@@ -1,10 +1,10 @@
-__title__ = "tablify-csv"
-__url__ = "https://github.com/ralphqq/tablify-csv"
-__license__ = "MIT License"
-__package__ = "tablify-csv"
-__version__ = "0.1.0"
-__short_description__ = (
-    "CLI tool that turns a CSV file into an HTML table with basic formatting"
-)
-__author__ = "Ralph Quirequire"
-__author_email__ = "ralph.quirequire2012@gmail.com"
+# fmt: off
+__title__ = 'tablify-csv'
+__url__ = 'https://github.com/ralphqq/tablify-csv'
+__license__ = 'MIT License'
+__package__ = 'tablify-csv'
+__version__ = '0.1.0'
+__short_description__ = 'CLI tool that turns a CSV file into an HTML table with basic formatting'
+__author__ = 'Ralph Quirequire'
+__author_email__ = 'ralph.quirequire2012@gmail.com'
+# fmt: on
